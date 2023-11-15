1b6dd306c21ed71ee6d0babe0dec2cfbe0c06f13
Yuri Terezin <127315162+YuriiTerezin@users.noreply.github.com>
2023-11-15 10:54:03 -0300

    Update CALCULO IMPOSTO DE RENDA.pas

--- CALCULO IMPOSTO DE RENDA.pas
+++ CALCULO IMPOSTO DE RENDA.pas
@@ -1,24 +1,24 @@
 Program IMPOSTO_RENDA ;
 var
-	Irbase1_1, Irbase1_2, Irbase2_1, Irbase2_2, Irbase3_1, Irbase3_2, Irbase4 : real;
-	Inbase1, Inbase2_1, Inbase2_2, Inbase3_1, Inbase3_2, Inbase4_1, Inbase4_2 : real;
+  Irbase1_1, Irbase1_2, Irbase2_1, Irbase2_2, Irbase3_1, Irbase3_2, Irbase4 : real;
+  Inbase1, Inbase2_1, Inbase2_2, Inbase3_1, Inbase3_2, Inbase4_1, Inbase4_2 : real;
 	
-	aliquota1, aliquota2, aliquota3, aliquota4 : real;
-	porcBase1, porcBase2, porcBase3, porcBase4 : real;
+  aliquota1, aliquota2, aliquota3, aliquota4 : real;
+  porcBase1, porcBase2, porcBase3, porcBase4 : real;
 	
-	resultBase1, resultBase2, resultBase3, resultBase4 : real;
-	deducao1, deducao2, deducao3, deducao4 : real;
+  resultBase1, resultBase2, resultBase3, resultBase4 : real;
+  deducao1, deducao2, deducao3, deducao4 : real;
 	
-	salario, ir, inss, ir2, valorIr, valorIr2, valorDep, dep, result : real;
-	liquido, irSimples, irFinal, valorFgts, fgts, valorTransp, vt : real;
-	dependentes : integer;
-	cont, transporte: char;
+ salario, ir, inss, ir2, valorIr, valorIr2, valorDep, dep, result : real;
+ liquido, irSimples, irFinal, valorFgts, fgts, valorTransp, vt : real;
+ dependentes : integer;
+ cont, transporte: char;
 
 Begin
 
-	repeat
+repeat
 
-//	Tabela de valores do IR
+// Tabela de valores do IR
   Irbase1_1 := 2112.01;
   Irbase1_2 := 2826.65;
   Irbase2_1 := 2826.66;
@@ -27,11 +27,11 @@ Begin
   Irbase3_2 := 4664.68;
   Irbase4   := 4665.69;
   
-//	Valores do Imposto Simplificado e de Dependentes
+// Valores do Imposto Simplificado e de Dependentes
   valorDep 	:= 189.59;
   irSimples := 528;
   
-//	Porcentagens de cada base
+// Porcentagens de cada base
   aliquota1 := 7.5  / 100;
   aliquota2 := 15   / 100;
   aliquota3 := 22.5 / 100;
@@ -43,7 +43,7 @@ Begin
   deducao3 := 651.73;
   deducao4 := 884.96;  
   
-//	Tabela de valores do INSS
+// Tabela de valores do INSS
   Inbase1   := 1320;
   Inbase2_1 := 1320.01;
   Inbase2_2 := 2571.29;
@@ -52,115 +52,115 @@ Begin
   Inbase4_1 := 3856.95;
   Inbase4_2 := 7507.49;
 
-//	Porcentagens de cada base do INSS
+// Porcentagens de cada base do INSS
   porcBase1 := 7.5 / 100;
   porcBase2 := 9   / 100;
   porcBase3 := 12  / 100;
   porcBase4 := 14  / 100;
   
-//	Valores para a progreção de desconto
+// Valores para a progreÃ§Ã£o de desconto
   resultBase1 :=  Inbase1 * porcBase1;
   resultBase2 := (Inbase2_2 - Inbase2_1) * porcBase2;
   resultBase3 := (Inbase3_2 - Inbase3_1) * porcBase3;
   resultBase4 := (Inbase4_2 - Inbase4_1) * porcBase4;
   
-//	Valor do FGTS
-	fgts := 8 / 100;
+// Valor do FGTS
+  fgts := 8 / 100;
 	
-//	Valor do Vale Transporte
-	vt := 6 / 100;
+// Valor do Vale Transporte
+  vt := 6 / 100;
     
-	writeln('Insira sua base de INSS e IR: ');
-  	read(salario);
+  writeln('Insira sua base de INSS e IR: ');
+    read(salario);
   	
   writeln('Possui dependentes? Insira a quantidade: ');
-  	read(dependentes);
+    read(dependentes);
   	
-  writeln('Você utiliza Vale Transporte? Digite " s " para confirmar: ');
-  	read(transporte);
+  writeln('VocÃª utiliza Vale Transporte? Digite " s " para confirmar: ');
+    read(transporte);
 
   	
-//	Cálculo do INSS
+// CÃ¡lculo do INSS
   if (salario <= Inbase1) then
-  	inss := salario * porcBase1
+    inss := salario * porcBase1
   else if (salario >= Inbase2_1) and (salario <= Inbase2_2) then
-  	inss := ((salario - Inbase2_1) * porcBase2) + resultBase1
+    inss := ((salario - Inbase2_1) * porcBase2) + resultBase1
   else if (salario >= Inbase3_1) and (salario <= Inbase3_2) then
-  	inss := ((salario - Inbase3_1) * porcBase3) + resultBase1 + resultBase2
+    inss := ((salario - Inbase3_1) * porcBase3) + resultBase1 + resultBase2
   else if (salario >= Inbase4_1) and (salario <= Inbase4_2) then
-  	inss := ((salario - Inbase4_1) * porcBase4) + resultBase1 + resultBase2 + resultBase3
+    inss := ((salario - Inbase4_1) * porcBase4) + resultBase1 + resultBase2 + resultBase3
   else if (salario >= Inbase4_2) then
-  	inss := resultBase1 + resultBase2 + resultBase3 + resultBase4;
+    inss := resultBase1 + resultBase2 + resultBase3 + resultBase4;
   	
-//	Desconto de Vale Transporte
-	if (transporte = 's') then
-		valorTransp := salario * vt
-	else
-		salario := salario;
+// Desconto de Vale Transporte
+  if (transporte = 's') then
+    valorTransp := salario * vt
+  else
+    salario := salario;
 		
-//	Multiplica valor dos dependentes pela quantidade	
+// Multiplica valor dos dependentes pela quantidade	
   dep := dependentes * valorDep; 
   
-//	Verifica se há dependentes para descontar
+// Verifica se hÃ¡ dependentes para descontar
   if (dependentes >= 1) then
-		ir := salario - inss - dep
-	else if (dependentes = 0) then
-		ir := salario - inss;
+    ir := salario - inss - dep
+  else if (dependentes = 0) then
+    ir := salario - inss;
 		
-//	Cálculo do Imposto de Renda
+// CÃ¡lculo do Imposto de Renda
   if (ir >= Irbase1_1) and (ir <= Irbase1_2) then
-  	valorIr := (ir * aliquota1) - deducao1
+    valorIr := (ir * aliquota1) - deducao1
   else if (ir >= Irbase2_1) and (ir <= Irbase2_2) then
-  	valorIr := (ir * aliquota2) - deducao2
+    valorIr := (ir * aliquota2) - deducao2
   else if (ir >= Irbase3_1) and (ir <= Irbase3_2) then
-  	valorIr := (ir * aliquota3) - deducao3
+    valorIr := (ir * aliquota3) - deducao3
   else if (ir >= Irbase4) then
-  	valorIr := (ir * aliquota4) - deducao4;
+    valorIr := (ir * aliquota4) - deducao4;
   	
-//	Cáculo do IR Simples
-	ir2 := salario - irSimples;
+// CÃ¡culo do IR Simples
+  ir2 := salario - irSimples;
 	
-	if (ir2 >= Irbase1_1) and (ir2 <= Irbase1_2) then
-	  valorIr2 := (ir2 * aliquota1) - deducao1
-	else if (ir2 >= Irbase2_1) and (ir2 <= Irbase2_2) then
-	  valorIr2 := (ir2 * aliquota2) - deducao2
-	else if (ir2 >= Irbase3_1) and (ir2 <= Irbase3_2) then
-	  valorIr2 := (ir2 * aliquota3) - deducao3
-	else if (ir2 >= Irbase4) then
-	  valorIr2 := (ir2 * aliquota4) - deducao4;
+  if (ir2 >= Irbase1_1) and (ir2 <= Irbase1_2) then
+    valorIr2 := (ir2 * aliquota1) - deducao1
+  else if (ir2 >= Irbase2_1) and (ir2 <= Irbase2_2) then
+    valorIr2 := (ir2 * aliquota2) - deducao2
+  else if (ir2 >= Irbase3_1) and (ir2 <= Irbase3_2) then
+    valorIr2 := (ir2 * aliquota3) - deducao3
+  else if (ir2 >= Irbase4) then
+    valorIr2 := (ir2 * aliquota4) - deducao4;
 	  
-//	Calculo do FGTS
-	valorFgts := salario * fgts;
+// Calculo do FGTS
+  valorFgts := salario * fgts;
 
-//	Verifica qual o mais vantajoso pra o funcionáio  	
-	if (valorIr > valorIr2) then
-	  irFinal := valorIr2
-	else
-	  irFinal := valorIr;
+// Verifica qual o mais vantajoso pra o funcionÃ¡io  	
+ if (valorIr > valorIr2) then
+   irFinal := valorIr2
+else
+  irFinal := valorIr;
+
+clrscr;
 	
-	clrscr;
-	
-	writeln('');	
-  writeln('Salário Base: R$ ', salario:0:2);
+  writeln('');	
+  writeln('SalÃ¡rio Base: R$ ', salario:0:2);
   
   writeln('');	
   writeln('Dependentes: ', dependentes);
   
   writeln('');
-	writeln('Desconto de Inss: ', inss:0:2);
+  writeln('Desconto de Inss: ', inss:0:2);
 	
-	writeln('');
-	writeln('Contribuição de FGTS: ', valorFgts:0:2);
+  writeln('');
+  writeln('ContribuiÃ§Ã£o de FGTS: ', valorFgts:0:2);
 	
-//	Caso não tenha desconto de IR, mostra que não há valor a ser descontado
-	writeln('');        
-	if (ir <= Irbase1_1) and (ir2 <= irBase1_1) then
-  	writeln('Não há desconto de Imposto de Renda.')
-	else	 	
-  	writeln('Desconto de Imposto de Renda: ', irFinal:0:2);
+// Caso nÃ£o tenha desconto de IR, mostra que nÃ£o hÃ¡ valor a ser descontado
+  writeln('');        
+  if (ir <= Irbase1_1) and (ir2 <= irBase1_1) then
+    writeln('NÃ£o hÃ¡ desconto de Imposto de Renda.')
+  else	 	
+    writeln('Desconto de Imposto de Renda: ', irFinal:0:2);
   	
   writeln('');
-	writeln('Desconto de Vale Transporte: ', valorTransp:0:2);
+  writeln('Desconto de Vale Transporte: ', valorTransp:0:2);
   
   result := inss + irFinal + valorTransp;
   
@@ -170,15 +170,15 @@ Begin
   liquido := salario - result;
   
   writeln('');
-  writeln('Salário líquido de: R$ ', liquido:0:2);
+  writeln('SalÃ¡rio lÃ­quido de: R$ ', liquido:0:2);
   
   writeln('');
 		
-	writeln('Deseja refazer o cálculo? Digite " s " para continuar...');
-	read(cont);
+  writeln('Deseja refazer o cÃ¡lculo? Digite " s " para continuar...');
+  read(cont);
 	
-	clrscr;
+  clrscr;
 	
-	until(cont <> 's');
+  until(cont <> 's');
      
 End.
